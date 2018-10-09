f189cb0053dee8e14920a65527b0e9ece3ea2376
denys <mystalowawola@gmail.com>
2018-10-09 15:37:05 +0200

    remove 13 byte for linux added

--- src/org/stepic/java/io/Main.java
+++ src/org/stepic/java/io/Main.java
@@ -7,16 +7,18 @@ public class Main {
     protected static InputStream in = System.in;
 
     public static void main(String[] args) throws IOException{
-//            System.setIn(new ByteArrayInputStream(new byte[] {65, 13, 10, 10, 13}));
-            int prev = in.read();
+            System.setIn(new ByteArrayInputStream(new byte[] {65, 13, 10, 10, 13}));
+            int prev = System.in.read();
             int next = 0;
             if (prev != -1) {
-                while ((next = in.read()) != -1) {
+                while ((next = System.in.read()) != -1) {
                     if ((prev != 13) || (next != 10)) {
                         out.write(prev);
-                        prev = next;
                     }
+                    prev = next;
                 }
+                out.write(prev);
             }
+            out.flush();
     }
 }
